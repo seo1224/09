f63bef778ee0fa8dc0454cee4718987697b9eb92
seo1224 <43231985+seo1224@users.noreply.github.com>
2018-11-29 16:04:40 +0900

    code change

--- main.c
+++ main.c
@@ -3,10 +3,51 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int is_whitespace(char c){
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '(' || c == ')'){
+		return 1;
+	}
+	return 0;	
+}
+	
+int fget_word(FILE* fp, char* word){
+	
+	char c;
+	int cnt;
+	
+	while ((c = fgetc(fp)) != EOF)
+	{
+		if(is_whitespace(c) == 0)
+		{
+			break;
+		}
+	}
+	if (c == EOF)
+	{
+		return 0; 
+	}
+	 
+	cnt = 0;
+	word[cnt++] = c;
+	word[cnt] = '\0';
+	
+	while ((word[cnt] = fgetc(fp)) != EOF){
+		if (is_whitespace(word[cnt])== 1)
+		{
+			word[cnt] = '\0';
+			break;
+		}
+		cnt++;
+	}
+	
+	return cnt; 
+}
+
 int main(int argc, char *argv[]) {
 	
 	FILE* fp;
 	char name[100];
+	char word[100];	
 	
 	printf("Input a file name :");
     scanf("%s", &name);
@@ -19,9 +60,15 @@ int main(int argc, char *argv[]) {
     	return -1;
 	}
     
-    printf("%c\n", fgetc(fp));
+    while (fget_word(fp, word) != 0)
+    {
+    	printf("%s\n", word);
+	}
     
     fclose(fp);
 	
 	return 0;
 }
+
+
+
